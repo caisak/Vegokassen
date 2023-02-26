195ef0f6c5134df8a4822c2e93611b780d844a45
mims <113519976+MimPMC@users.noreply.github.com>
2023-02-26 11:10:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "./Components/Footer";
 import { Header } from "./Components/Header";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-import { faCartShopping, faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
-
-library.add(faCheckSquare, faCoffee, faCartShopping)
-
-
 
 export function App() {
   return(
