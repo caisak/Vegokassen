08a8911a831f105afe05b519625dc50d5fb7a3e4
mims <113519976+MimPMC@users.noreply.github.com>
2023-02-27 15:50:31 +0100

    carousel styling

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -1,62 +1,153 @@
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import styled from "styled-components";
 
-interface CarouselProps {
+interface Props {
   images: string[];
+  height: number;
 }
 
+export function Carousel(props: Props) {
 
-
-export function Carousel({ images }: CarouselProps) {
-
+  const { images, height } = props;
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrevClick = () => {
     const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
-    console.log("prevImg")
   };
 
   const handleNextClick = () => {
-    const newIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-    console.log("nextImg")
+    const nextIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+    setCurrentIndex(nextIndex);
   };
 
   return (
-    <Container>
-      <Image src={images[currentIndex]} alt="Carousel Image" />
+    <Container images={images} height={height}>
       <ButtonWrapper>
-        <button onClick={handlePrevClick}>Previous</button>
-        <button onClick={handleNextClick}>Next</button>
+        <FontAwesomeIcon onClick={handlePrevClick} icon={faChevronLeft} />
+        <FontAwesomeIcon  onClick={handleNextClick} icon={faChevronRight}/>
       </ButtonWrapper>
+      <ContentWrapper>
+        <InfoBox>
+          <Title>Vi gör vegetarisk matlagning enkelt och inspirerande!</Title>
+          <ButtonBox>
+            <MainWhiteButton>Våra Kassar</MainWhiteButton>
+            <MainButton>Så funkar det!</MainButton>
+          </ButtonBox>
+        </InfoBox>
+      </ContentWrapper>
+      <Image src={images[currentIndex]} alt="Carousel Image" />
     </Container>
   );
 }
 
-const Container = styled.div`
-  height: 40vh;
-  width: 100%;
-  position: relative;
-  @media (max-width: 700px) {
-    height: 30vh;
-  }
-`;
-
 const Image = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
   height: 100%;
   width: 100%;
   object-fit: cover;
   object-position: 50% 50%;
+`;
+
+const Container = styled.div<Props>`
+  height: ${(props) => `${props.height}vh`};
+  width: 100%;
+  position: relative;
   @media (max-width: 700px) {
-    object-fit: cover;
-    object-position: 50% 50%;
+    height: ${(props) => `${props.height - 10}vh`};
   }
 `;
 
 const ButtonWrapper = styled.div`
+z-index: 300;
   position: absolute;
-  bottom: 10px;
-  left: 50%;
-  transform: translateX(-50%);
+  top: 50%;
+  left: 0;
+  right: 0;
+  transform: translateY(-50%);
+  display: flex;
+  justify-content: space-between;
+
+  & > * {
+    /* Apply styles to all direct children */
+    color: white;
+    cursor: pointer;
+    font-size: 1.2rem;
+    background-color: rgba(0, 31, 14, 0.3);;
+    border-radius: 50%;
+    aspect-ratio: 1;
+    padding: .3rem .3rem;
+  }
 `;
+const ContentWrapper = styled.div`
+  z-index: 200;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(to top, rgba(17, 40, 24, 0.85), transparent);
+`;
+
+const InfoBox = styled.div `
+  display: flex;
+  flex-direction: column;
+  margin:0;
+  position: absolute;
+  top: 68%;
+  left: 7%;
+  font-family: "Titillium Web", sans-serif;
+`
+const Title = styled.h2 `
+  color: white;
+`
+
+const ButtonBox = styled.div `
+display: flex;
+width: 100%;
+flex-direction: row;
+justify-content: flex-start;
+`
+
+const MainButton = styled.button `
+  border-radius: .2rem;
+  font-weight: 500;
+  width: 30%;
+  margin-right: 5%;
+  background-color: #F8E1A6;
+  border: none;
+  font-family: "Titillium Web", sans-serif;
+  color: #105434;
+  transition: 0.2s;
+  &:hover {
+    background-color: #c5b281;
+  }  
+  &:active {
+    transform: scale(0.8);
+    box-shadow: 0 0 5px black;
+  }
+  
+`
+const MainWhiteButton = styled.button `
+  border-radius: .2rem;
+  font-weight: 500;
+  width: 30%;
+  margin-right: 5%;
+  background-color: #ffffff;
+  border: none;
+  font-family: "Titillium Web", sans-serif;
+  color: #105434;
+  transition: 0.2s;
+  &:hover {
+    background-color: #cfcfcf;
+  }  
+  &:active {
+    transform: scale(0.8);
+    box-shadow: 0 0 5px black;
+  }
+`
+
+
+
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,7 +3,7 @@ import { Carousel } from "../Components/Carousel";
 export function Products() {
   return (
     <div>
-      <Carousel images={["src/Images/food_1.jpg","src/Images/food_2.jpg"]}></Carousel>
+      <Carousel height={50} images={["src/Images/food_1.jpg","src/Images/food_2.jpg"]} />
     </div>
   )
 }
