5242b12bbd7ffb5de0db6bc50ac4bce7b8f84e82
felicia <feliciawillnas@gmail.com>
2023-02-27 16:30:09 +0100

    recipecard component

---
+++ src/Components/RecipeCard.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export type RecipeCardProps = {
+  image: string;
+  title: string;
+  description: string;
+};
+
+export const RecipeCard = ({ image, title, description }: RecipeCardProps) => {
+  return (
+    <StyledRecipeCardDiv>
+      <StyledRecipeCardWrapper>
+        <StyledRecipeCardImage src={image} alt={title} />
+        <StyledRecipeCardTitle>{title}</StyledRecipeCardTitle>
+        <StyledRecipeCardDesc>{description}</StyledRecipeCardDesc>
+      </StyledRecipeCardWrapper>
+    </StyledRecipeCardDiv>
+  );
+};
+
+const StyledRecipeCardDiv = styled.div `
+  box-shadow: 0px 4px 2px #dfd7d7;
+  margin: 10px;
+`;
+
+const StyledRecipeCardWrapper = styled.div `
+  display: flex;
+  flex-direction: column; 
+  width: 300px;
+  background-color: white;
+`;
+
+const StyledRecipeCardImage = styled.img `
+  width: 100%; 
+  height: 400px;
+  object-fit: cover;
+`;
+
+const StyledRecipeCardTitle = styled.h2 `
+  text-align: center;
+  color: #392E7C;
+  font-size: 18px;
+  padding: 20px 10px 0px 10px;
+  font-family: 'Ubuntu Condensed', sans-serif;
+`;
+
+const StyledRecipeCardDesc = styled.p `
+  color: #392E7C;
+  font-size: 16px;
+  height: 75px;
+  padding: 10px 10px 20px 10px;
+  font-family: 'Titillium Web', sans-serif;
+  text-align: center;
+`;
