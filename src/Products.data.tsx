3ee364e08aa9db7de0dd62d80d9f36837f832a8e
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-03-01 14:46:41 +0100

    changed to style in product file

--- src/Products.data.tsx
+++ src/Products.data.tsx
@@ -7,28 +7,28 @@ export interface Product {
 
 const ProductsList: Product[] = [
   {
-    name: "Klassisk och inspirerande",
+    name: "Familjekassen",
     image: "src/Images/bag_1.jpg",
     price: 499,
-    description: "text om kassen",
+    description: "Denna fantastiska matkasse är skapad för den upptagna familjen som älskar planeten och sig själva",
   },
   {
-    name: "Enkelt och lättlagat",
+    name: "PlantPower™kassen",
     image: "src/Images/bag_transparent.png",
-    price: 399,
-    description: "text om kassen",
+    price: 599,
+    description: "Recept och ingredienser med extra protein för den som vill bli starkare utan att försvaga planeten.",
   },
   {
-    name: "Barnfamilj",
+    name: "15-minuterskassen",
     image: "src/Images/groceries_2.jpg",
-    price: 599,
-    description: "text om kassen",
+    price: 399,
+    description: "Värna om planeten, er tid och er hälsa utan att kompromissa! Specialgjord med rätter som bara tar 15 minuter att laga. Hej då snabbmat, välkommen riktigt snabb mat!",
   },
   {
-    name: "placeholder",
+    name: "Fruktkassen",
     image: "src/Images/recipe7.jpg",
-    price: 599,
-    description: "text om kassen",
+    price: 199,
+    description: "Tillgodose veckans behov av vitaminer med vår populära fruktkasse.",
   },
 ];
 
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import styled from "styled-components";
 import { CartContext } from "../CartContex";
 import { StyledRecipeCardDiv, StyledRecipeCardImage, StyledRecipeCardWrapper } from "../Components/RecipeCard";
 import ProductsList, { Product } from "../Products.data";
@@ -13,20 +14,112 @@ export function Products() {
   }
 
   return (
-      <StyledRecipeCardsContainer>
+      <StyledProductContainer>
         {ProductsList.map((product) => (
-          <StyledRecipeCardDiv key={product.image}>
-            <StyledRecipeCardWrapper>
-            <h2>{product.name}</h2>
-            <StyledRecipeCardImage src={product.image} alt={product.name} />
+          <StyledDiv key={product.image}>
+            <StyledProductWrapper>
+            <StyledProductHeader>{product.name}</StyledProductHeader>
+            <StyledProductImage src={product.image} alt={product.name} />
+            <StyledProductText>{product.description}</StyledProductText>
             <p>{product.price} kr</p>
-            <p>{product.description}</p>
-            <button onClick={() => handleAddToCart(product)}>Add to cart</button>
-            </StyledRecipeCardWrapper>
-          </StyledRecipeCardDiv>
+            <StyledPutInCartButton onClick={() => handleAddToCart(product)}>Lägg till i kundvagn</StyledPutInCartButton>
+            </StyledProductWrapper>
+          </StyledDiv>
       ))}
-      </StyledRecipeCardsContainer>
+      </StyledProductContainer>
       );
     }
     
+  /*
+   align-items: center;
+    height: 80vh;
+    width: 28rem;
+    box-shadow: 0 0 10px #c0c0c0;
+    background-color: #ffff;
+    margin: 1rem;
+    border-radius: 0.5rem;
+    font-family: "Titillium Web", sans-serif;
+  */
+
+    export const StyledProductWrapper = styled.div `
+    display: flex;
+    flex-direction: column; 
+    width: 300px;
+    background-color: white;
+    box-shadow: 0px 4px 2px #dfd7d7;
+    cursor: pointer;
+  `;
+
+    const StyledProductContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    
+    @media(max-width:850px){
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: 0;
+    margin-right: 0;
+  }
+  
+  @media (min-width: 851px) and (max-width: 1000px) {
+      display: flex;
+      justify-content: space-between;
+      margin-left: 100px;
+      margin-right: 100px;
+    }
+  `;
+
+export const StyledDiv = styled.div `
+margin: 10px;
+padding-bottom: 30px;
+`;
+
+    const StyledProductDiv = styled.div`
+    margin: 1rem;
+    border-radius: 0.5rem;
+    height: 80vh;
+    width: 28rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: "Titillium Web", sans-serif;
+  `;
+  
+  const StyledProductHeader = styled.h2`
+  margin: 0.5rem;
+  `;
+  
+  const StyledProductImage = styled.img`
+  object-fit: cover;
+  height: 25rem;
+  width: 100%;
+  margin-bottom: 0.5rem;
+  `;
+  
+  const StyledProductText = styled.p`
+  margin: 0.5rem;
+  text-align: center;
+  `;
+
+  
+  const StyledPutInCartButton = styled.button`
+  width: 95%;
+  height: 2rem;
+  background-color: pink;
+  border-radius: 0.5rem;
+  border: none;
+  margin: 0.5rem;
+  
+  &:hover {
+      background-color: #C79EB6;
+  }
+  
+  &:active {
+      transform: scale(0.99);
+      box-shadow: 0 0 5px black;
+  }
+  `;
 
