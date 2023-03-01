d9496cf1fb9ae18017a1de298137b3e022c0c0d6
mims <113519976+MimPMC@users.noreply.github.com>
2023-03-01 14:41:37 +0100

    test1

--- src/Components/ApiTest.tsx
+++ src/Components/ApiTest.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 interface PopulationData {
   year: number;
@@ -10,30 +11,45 @@ const MyComponent = () => {
 
   useEffect(() => {
     const fetchPopulationData = async () => {
-      const response = await fetch('https://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy', {
-        method: 'POST',
-        body: JSON.stringify({
-          query: [
-            {
-              code: 'ContentsCode',
-              selection: {
-                filter: 'item',
-                values: ['BE0101N1'],
+      const response = await fetch(
+        "https://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            query: [
+              {
+                code: "ContentsCode",
+                selection: {
+                  filter: "item",
+                  values: ["BE0101N1"],
+                },
               },
-            },
-            {
-              code: 'Tid',
-              selection: {
-                filter: 'item',
-                values: ['2011', '2012','2013',],
+              {
+                code: "Tid",
+                selection: {
+                  filter: "item",
+                  values: [
+                    "2011",
+                    "2012",
+                    "2013",
+                    "2014",
+                    "2015",
+                    "2016",
+                    "2017",
+                    "2018",
+                    "2019",
+                    "2020",
+                    "2021",
+                  ],
+                },
               },
+            ],
+            response: {
+              format: "json",
             },
-          ],
-          response: {
-            format: 'json',
-          },
-        }),
-      });
+          }),
+        }
+      );
 
       const data = await response.json();
 
@@ -48,8 +64,9 @@ const MyComponent = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Population data for Sweden</h1>
+    <VeganIsTheFuture>
+        <div>
+        <StyledH2>Population data for Sweden</StyledH2>
       <ul>
         {populationData.map((data) => (
           <li key={data.year}>
@@ -58,8 +75,33 @@ const MyComponent = () => {
           </li>
         ))}
       </ul>
-    </div>
+        </div>
+        <div>
+
+        </div>
+
+    </VeganIsTheFuture>
   );
 };
+
+const VeganIsTheFuture = styled.div`
+  background-color: #8ac98a;
+  border-radius: 1rem;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  font-family: "Titillium Web", sans-serif;
+  display: flex;
+
+  div {
+
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+const StyledH2 = styled.h2`
+  font-family: "Ubuntu Condensed", sans-serif;
+`;
 
 export default MyComponent;
--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -1,17 +1,11 @@
-<<<<<<< Updated upstream
-=======
 import MyComponent from "../Components/ApiTest";
 import { ErrorBoundary } from "../Components/ErrorBoundary";
-import { ReviewCards } from "../Components/Review";
 
->>>>>>> Stashed changes
+                                        
 export function HowItWorks() {
   
     return (
       <ErrorBoundary>
-      <div>
-        <h1>This will be the How it works page</h1>
-      </div>
       <MyComponent></MyComponent>
       </ErrorBoundary>
     )
