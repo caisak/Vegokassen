38bdb66ae6eb73efdd302f74211024c756a043f3
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-03-01 18:52:04 +0100

    add broadhowcard file

---
+++ src/Components/BroadHowCard.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+interface Props {
+
+}
+
+export function BroadHowCard(props: Props) {
+  return (
+  <StyledBigDiv>
+    <img />
+    <h2>
+    hassa
+    </h2>
+    <p>
+
+    </p>
+
+  </StyledBigDiv>
+  );
+}
+
+const StyledBigDiv = styled.div`
+background-color: hotpink;
+height: 50vh;
+width: 100%;
+`
